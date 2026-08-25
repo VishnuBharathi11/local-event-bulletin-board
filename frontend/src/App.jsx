98703cd503e8af5c09fb0613d99c26460c35f359
VishnuBharathi11 <vishnubharathi1011@gmail.com>
2026-08-26 04:53:00 +0530

    feat: mount centralized authentication provider

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,13 @@
 import { BrowserRouter } from 'react-router-dom'
+import { AuthProvider } from './context/AuthContext.jsx'
 import AppRouter from './navigation/AppRouter.jsx'
 
 export default function App() {
   return (
     <BrowserRouter>
-      <AppRouter />
+      <AuthProvider>
+        <AppRouter />
+      </AuthProvider>
     </BrowserRouter>
   )
 }
