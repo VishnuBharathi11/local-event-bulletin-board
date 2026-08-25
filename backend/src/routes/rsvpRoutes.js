aab3b9401ab150050653d1818a7fd7619cca7b02
VishnuBharathi11 <vishnubharathi1011@gmail.com>
2026-08-26 04:51:16 +0530

    feat: protect RSVP endpoints

--- backend/src/routes/rsvpRoutes.js
+++ backend/src/routes/rsvpRoutes.js
@@ -1,10 +1,11 @@
 const express = require('express')
 const { getRSVPStatus, createRSVP, deleteRSVP } = require('../controllers/rsvpController')
+const { authenticate } = require('../middleware/authMiddleware')
 
 const router = express.Router()
 
-router.get('/events/:eventId/rsvp', getRSVPStatus)
-router.post('/events/:eventId/rsvp', createRSVP)
-router.delete('/events/:eventId/rsvp', deleteRSVP)
+router.get('/events/:eventId/rsvp', authenticate, getRSVPStatus)
+router.post('/events/:eventId/rsvp', authenticate, createRSVP)
+router.delete('/events/:eventId/rsvp', authenticate, deleteRSVP)
 
 module.exports = router
