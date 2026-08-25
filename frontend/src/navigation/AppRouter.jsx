4802a730465c6fe046825104daa217391fdf2dc8
VishnuBharathi11 <vishnubharathi1011@gmail.com>
2026-08-26 03:02:59 +0530

    feat: connect calendar route

--- frontend/src/navigation/AppRouter.jsx
+++ frontend/src/navigation/AppRouter.jsx
@@ -4,6 +4,7 @@ import PlaceholderPage from '../pages/PlaceholderPage.jsx'
 import EventBoardPage from '../pages/EventBoardPage.jsx'
 import CreateEventPage from '../pages/CreateEventPage.jsx'
 import EventDetailsPage from '../pages/EventDetailsPage.jsx'
+import CalendarPage from '../pages/CalendarPage.jsx'
 
 export default function AppRouter() {
   return (
@@ -12,7 +13,7 @@ export default function AppRouter() {
         <Route path="/" element={<EventBoardPage />} />
         <Route path="/events/new" element={<CreateEventPage />} />
         <Route path="/events/:eventId" element={<EventDetailsPage />} />
-        <Route path="/calendar" element={<PlaceholderPage title="Calendar" description="Calendar functionality will be implemented in a later migration phase." />} />
+        <Route path="/calendar" element={<CalendarPage />} />
         <Route path="/community-requests" element={<PlaceholderPage title="Community Requests" description="Community request functionality will be implemented later." />} />
         <Route path="/community-requests/new" element={<PlaceholderPage title="Request Event" description="Event request creation will be implemented later." />} />
         <Route path="/community-requests/:requestId" element={<PlaceholderPage title="Request Details" description="Request details will be implemented later." />} />
