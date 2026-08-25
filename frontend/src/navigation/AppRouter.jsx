f7c086fada507b646d34966907fb723fb274d706
VishnuBharathi11 <vishnubharathi1011@gmail.com>
2026-08-26 03:10:23 +0530

    feat: implement demand driven event confirmation

--- frontend/src/navigation/AppRouter.jsx
+++ frontend/src/navigation/AppRouter.jsx
@@ -1,10 +1,12 @@
 import { Navigate, Route, Routes } from 'react-router-dom'
 import AppShell from '../layouts/AppShell.jsx'
-import PlaceholderPage from '../pages/PlaceholderPage.jsx'
 import EventBoardPage from '../pages/EventBoardPage.jsx'
 import CreateEventPage from '../pages/CreateEventPage.jsx'
 import EventDetailsPage from '../pages/EventDetailsPage.jsx'
 import CalendarPage from '../pages/CalendarPage.jsx'
+import CommunityRequestsPage from '../pages/CommunityRequestsPage.jsx'
+import CreateEventRequestPage from '../pages/CreateEventRequestPage.jsx'
+import EventRequestDetailsPage from '../pages/EventRequestDetailsPage.jsx'
 
 export default function AppRouter() {
   return (
@@ -14,9 +16,9 @@ export default function AppRouter() {
         <Route path="/events/new" element={<CreateEventPage />} />
         <Route path="/events/:eventId" element={<EventDetailsPage />} />
         <Route path="/calendar" element={<CalendarPage />} />
-        <Route path="/community-requests" element={<PlaceholderPage title="Community Requests" description="Community request functionality will be implemented later." />} />
-        <Route path="/community-requests/new" element={<PlaceholderPage title="Request Event" description="Event request creation will be implemented later." />} />
-        <Route path="/community-requests/:requestId" element={<PlaceholderPage title="Request Details" description="Request details will be implemented later." />} />
+        <Route path="/community-requests" element={<CommunityRequestsPage />} />
+        <Route path="/community-requests/new" element={<CreateEventRequestPage />} />
+        <Route path="/community-requests/:requestId" element={<EventRequestDetailsPage />} />
         <Route path="*" element={<Navigate to="/" replace />} />
       </Route>
     </Routes>
