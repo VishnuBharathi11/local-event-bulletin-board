c713c378885fdadd1bb00e3c644ee2fe7ccf02c2
VishnuBharathi11 <vishnubharathi1011@gmail.com>
2026-08-26 04:54:14 +0530

    refactor: remove development-user authentication abstraction

--- backend/src/config/developmentUser.js
+++
@@ -1,7 +0,0 @@
-const DEVELOPMENT_USER_ID = process.env.DEVELOPMENT_USER_ID || 'dev_user'
-
-function getDevelopmentUserId() {
-  return DEVELOPMENT_USER_ID
-}
-
-module.exports = { getDevelopmentUserId }
