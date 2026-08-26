5b75debd42419f29c0303e2eefe41eaadec85252
VishnuBharathi11 <vishnubharathi1011@gmail.com>
2026-08-26 15:08:50 +0530

    style: finalize EventHive visual consistency

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,6 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import './styles/auth.css'
 import './styles/responsiveAccessibilityUi08.css'
+import './styles/visualConsistencyUi09.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
